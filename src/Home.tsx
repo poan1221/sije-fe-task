ce92511c1ea889519bf1b0aed921cee0e125417f
haSong <70552536+poan1221@users.noreply.github.com>
2025-11-30 20:45:32 +0900

    style: sije 스타일 적용 - 로고 및 파비콘 설정

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,6 +3,13 @@ import { PaymentTable } from "@/payment-table/PaymentTable";
 export function Home() {
   return (
     <>
+      <img
+        src="/src/images/sije-logo.png"
+        alt="로고 이미지"
+        width="70"
+        height="25"
+        className="object-contain"
+      />
       <PaymentTable />
     </>
   );
